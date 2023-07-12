44c1c59e33e7d7b42baea7907ef65e8fdb72d9e9
李宁 <lining06@bilibili.com>
2023-07-12 16:14:02 +0800

    feat: 配置文件解析和热更新

--- src/runtime/client-entry.tsx
+++ src/runtime/client-entry.tsx
@@ -1,11 +1,13 @@
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import siteData from 'island:site-data';
 
 // 浏览器端入口文件，用于渲染主题组建
 function renderInBrowser() {
-  const containerEl = document.getElementById("root");
+  console.log(siteData);
+  const containerEl = document.getElementById('root');
   if (!containerEl) {
-    throw new Error("#root element not found");
+    throw new Error('#root element not found');
   }
   createRoot(containerEl).render(<App />);
 }
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entryPoints: ["src/node/cli.ts"],
+  entryPoints: {
+    cli: './src/node/cli.ts',
+    index: './src/node/index.ts',
+    dev: './src/node/dev.ts'
+  },
+  clean: true, // 清空之前的构建产物
   bundle: true,
   splitting: true,
-  minify: process.env.NODE_ENV === "production",
-  outDir: "dist",
-  format: ["cjs", "esm"],
+  minify: process.env.NODE_ENV === 'production',
+  outDir: 'dist',
+  format: ['cjs', 'esm'],
   dts: true,
-  shims: true,
-  banner: {
-    js: 'import { createRequire as createRequire0 } from "module"; const require = createRequire0(import.meta.url);'
-  },
+  shims: true
 });
