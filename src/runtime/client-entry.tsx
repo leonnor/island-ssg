99045490acfae56c19c8364c4f9438a209327720
Leonnor <958978379@qq.com>
2023-07-13 23:22:02 +0800

    feat: routerDemo开发

--- src/runtime/client-entry.tsx
+++ src/runtime/client-entry.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import siteData from 'island:site-data';
+import { BrowserRouter } from 'react-router-dom';
 
 // 浏览器端入口文件，用于渲染主题组建
 function renderInBrowser() {
@@ -9,7 +10,11 @@ function renderInBrowser() {
   if (!containerEl) {
     throw new Error('#root element not found');
   }
-  createRoot(containerEl).render(<App />);
+  createRoot(containerEl).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 }
 
 renderInBrowser();
